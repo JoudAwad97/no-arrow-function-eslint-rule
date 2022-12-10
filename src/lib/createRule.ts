d0427fc844bd26c5b9276d3527a437755dd7a88a
Joud Awad <joud.awad@metalabdesign.com>
2022-12-10 16:19:12 +0300

    Updates

--- src/lib/createRule.ts
+++
@@ -1,6 +0,0 @@
-import { ESLintUtils } from "@typescript-eslint/utils";
-
-export const createRule = ESLintUtils.RuleCreator(
-  () =>
-    "https://github.com/JoudAwad97/no-arrow-function-eslint-rule/blob/7cd6339a822fbb47f0819fa8c4773111d15a4a17/README.md"
-);
---
+++ src/rules/disallow-arrow-functions.js
@@ -0,0 +1,14 @@
+module.exports = {
+  create(context) {
+    return {
+      VariableDeclarator(node) {
+        if (node.init && node.init.type === "ArrowFunctionExpression") {
+          context.report(
+            node,
+            "Arrow functions are disallowed use traditional functions instead"
+          );
+        }
+      },
+    };
+  },
+};
