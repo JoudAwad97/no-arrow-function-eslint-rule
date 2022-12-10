eb3fcf7cc18c9eb925c98bf8ba13bfa43e8ba6f3
Joud Awad <joud.awad@metalabdesign.com>
2022-12-10 15:39:03 +0300

    inital files

---
+++ index.js
@@ -0,0 +1,14 @@
+/**
+ * @fileoverview disallow-arrow-functions
+ * @author Joud Awad
+ */
+"use strict";
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+const requireIndex = require("requireindex");
+//------------------------------------------------------------------------------
+// Plugin Definition
+//------------------------------------------------------------------------------
+// import all rules in lib/rules
+module.exports.rules = requireIndex(__dirname + "/rules");
